8f423139fb96c4c533a0f4cf3c54b34d3b10dd43
Umair Khan <umair.waheed@gmail.com>
2025-03-16 23:26:06 +0500

    create vector extension

--- app/db.py
+++ app/db.py
@@ -1,8 +1,12 @@
 import os
 
+from sqlalchemy import text
 from sqlmodel import SQLModel, create_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
+
+with engine.connect() as conn:
+    conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
 
 SQLModel.metadata.create_all(engine)
