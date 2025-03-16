f4f32570d7cad5533db6353cbe3792abb9ab5cce
Umair Khan <umair.waheed@gmail.com>
2025-03-16 23:23:26 +0500

    remove sqlite default

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ import os
 
 from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./db.sqlite3")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 SQLModel.metadata.create_all(engine)
