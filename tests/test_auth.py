da5b2408a2710e5aff2aa2cdd8c4201a510873af
Umair Khan <umair.waheed@gmail.com>
2025-03-17 05:44:37 +0500

    add security test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,7 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.helpers import ALGORITHM, SECRET_KEY
-from app.models import User
+from app.models import Role, User
 
 
 def test_login_for_access_token(client: TestClient, test_user: User):
@@ -22,3 +22,10 @@ def test_post_register(client: TestClient, test_user_data: dict):
     response = client.post("/auth/register/", json=test_user_data)
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["username"] == test_user_data["username"]
+
+
+def test_post_register_cannot_set_admin_role(client: TestClient, test_user_data: dict):
+    test_user_data["role"] = Role.ADMIN
+    response = client.post("/auth/register/", json=test_user_data)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["role"] == Role.USER
